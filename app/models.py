5439fbef02753a1fd0d20ac57e1f9fe206b930e4
Stekloduv <Stekloduv45@gmail.com>
2025-11-05 08:20:43 +0100

    fix name and media name bug

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Friend(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    neme = models.TextField(blank=False, null=False)
+    name = models.TextField(blank=False, null=False)
     profession = models.TextField(blank=False, null=False)
     profession_description = models.TextField(blank=True, null=True)
     photo_url = models.ImageField(upload_to='media/', null=True, blank=True)
